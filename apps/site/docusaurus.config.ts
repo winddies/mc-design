50f2b83686f0c1db91b26c1c99f419521ec4fee0
Heng-Zhang2 <Heng.Zhang2@budweiserapac.com>
2024-10-27 22:22:48 +0800

    fix: delete useless route

--- apps/site/docusaurus.config.ts
+++ apps/site/docusaurus.config.ts
@@ -117,10 +117,6 @@ const config: Config = {
           title: 'More',
           items: [
             {
-              label: 'Blog',
-              to: '/blog',
-            },
-            {
               label: 'GitHub',
               href: 'https://github.com/facebook/docusaurus',
             },
